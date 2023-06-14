252b58e53cfe59779966ff3e3ee3fed0a48ed91c
오채영 <web3765.gmail.com>
2023-06-14 21:54:50 +0900

    [feat] MemberDto 생성

---
+++ src/main/java/com/sweetievegan/domain/dto/MemberDto.java
@@ -0,0 +1,28 @@
+package com.sweetievegan.domain.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class MemberDto {
+    private Long memberId;
+    private String loginId;
+    private String pw;
+    private String nickname;
+    private boolean authorization;
+    private boolean subscribe;
+    private boolean avail;
+    private String profileImg;
+
+    @Builder
+    public MemberDto(Long memberId, String loginId, String pw, String nickname, boolean authorization, boolean subscribe, boolean avail, String profileImg) {
+        this.memberId = memberId;
+        this.loginId = loginId;
+        this.pw = pw;
+        this.nickname = nickname;
+        this.authorization = authorization;
+        this.subscribe = subscribe;
+        this.avail = avail;
+        this.profileImg = profileImg;
+    }
+}
