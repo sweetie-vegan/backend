d9c43fbbb23a833f5e19c67eac04d7357fbfa405
오채영 <web3765.gmail.com>
2023-06-15 19:14:57 +0900

    [feat] ProductEntity - Builder annotation 추가

--- src/main/java/com/sweetievegan/domain/entity/ProductEntity.java
+++ src/main/java/com/sweetievegan/domain/entity/ProductEntity.java
@@ -1,6 +1,7 @@
 package com.sweetievegan.domain.entity;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,6 +22,7 @@ public class ProductEntity {
     private int count;
     private boolean sale;
 
+    @Builder
     public ProductEntity(Long productId, String productName, String productDescription, int price, int count, boolean sale) {
         this.productId = productId;
         this.productName = productName;
