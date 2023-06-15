106df3f328b260be4666b5e0754a9a588762edb0
오채영 <web3765.gmail.com>
2023-06-15 13:45:25 +0900

    [feat] annotation 추가

--- src/main/java/com/sweetievegan/domain/dto/ProductDto.java
+++ src/main/java/com/sweetievegan/domain/dto/ProductDto.java
@@ -1,9 +1,14 @@
 package com.sweetievegan.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDto {
     private Long productId;
     private String productName;
@@ -11,14 +16,4 @@ public class ProductDto {
     private int price;
     private int count;
     private boolean sale;
-
-    @Builder
-    public ProductDto(Long productId, String productName, String productDescription, int price, int count, boolean sale) {
-        this.productId = productId;
-        this.productName = productName;
-        this.productDescription = productDescription;
-        this.price = price;
-        this.count = count;
-        this.sale = sale;
-    }
 }
--- src/main/java/com/sweetievegan/domain/repository/ProductRepository.java
+++ src/main/java/com/sweetievegan/domain/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.sweetievegan.domain.repository;
 
 import com.sweetievegan.domain.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 }
