8d8378639448c2c61da85fa1fe4713740bd90871
오채영 <web3765.gmail.com>
2023-06-17 15:52:55 +0900

    [refac] OrderEntity - table 이름 수정

--- src/main/java/com/sweetievegan/domain/entity/OrderEntity.java
+++ src/main/java/com/sweetievegan/domain/entity/OrderEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "order")
+@Table(name = "orders")
 public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
