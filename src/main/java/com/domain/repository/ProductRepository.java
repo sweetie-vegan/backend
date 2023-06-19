6c62a6a7929d01249c48e89b525ffee69c4f2e40
오채영 <web3765.gmail.com>
2023-06-19 11:28:36 +0900

    [feat] ProductSpecification 생성 - search 목적

--- src/main/java/com/domain/repository/ProductRepository.java
+++ src/main/java/com/domain/repository/ProductRepository.java
@@ -2,10 +2,11 @@ package com.domain.repository;
 
 import com.domain.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>, JpaSpecificationExecutor<ProductEntity> {
 
     ProductEntity findProductByProductId(Long productId);
 }
---
+++ src/main/java/com/domain/specification/ProductSpecification.java
@@ -0,0 +1,37 @@
+package com.domain.specification;
+
+import com.domain.entity.ProductEntity;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public interface ProductSpecification {
+    public static Specification<ProductEntity> findProductByNameKeyword(String nameKeyword) {
+        return new Specification<ProductEntity>() {
+            @Override
+            public Predicate toPredicate(Root<ProductEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.like(root.get("productName"), "%" + nameKeyword + "%");
+            }
+        };
+    }
+    public static Specification<ProductEntity> findProductByIngredientKeyword(String ingredientKeyword) {
+        return new Specification<ProductEntity>() {
+            @Override
+            public Predicate toPredicate(Root<ProductEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.like(root.get("productDescription"), "%" + ingredientKeyword + "%");
+            }
+        };
+    }
+    public static Specification<ProductEntity> betweenPrice(int price) {
+        return new Specification<ProductEntity>() {
+            @Override
+            public Predicate toPredicate(Root<ProductEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.between(root.get("price"), 0, price);
+            }
+        };
+    }
+    
+}
