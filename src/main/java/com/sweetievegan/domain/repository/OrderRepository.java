fe68f9c526a432453890a2a480e538fa67c109ec
오채영 <web3765.gmail.com>
2023-06-19 13:25:01 +0900

    [feat] OrderRepository - find 추가

--- src/main/java/com/sweetievegan/domain/repository/OrderRepository.java
+++ src/main/java/com/sweetievegan/domain/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package com.sweetievegan.domain.repository;
 import com.sweetievegan.domain.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    OrderEntity findOrderByOrderId(Long OrderId);
+    List<OrderEntity> findOrdersByMemberLoginId(String loginId);
 }
