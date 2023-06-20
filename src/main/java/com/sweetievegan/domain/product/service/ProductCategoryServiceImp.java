a61277a77f0169c81af79079f8700394bec6a739
오채영 <web3765.gmail.com>
2023-06-20 20:48:26 +0900

    [feat] ProductCategoryService - 생성자 주입

--- src/main/java/com/sweetievegan/domain/product/service/ProductCategoryServiceImp.java
+++ src/main/java/com/sweetievegan/domain/product/service/ProductCategoryServiceImp.java
@@ -2,12 +2,19 @@ package com.sweetievegan.domain.product.service;
 
 import com.sweetievegan.domain.product.dto.ProductCategoryDto;
 import com.sweetievegan.domain.product.dto.ProductDto;
+import com.sweetievegan.domain.product.repository.ProductCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class ProductCategoryServiceImp implements ProductCategoryService {
+    private final ProductCategoryRepository productCategoryRepository;
     @Override
     public List<ProductCategoryDto> getAllProductCategories() {
+        productCategoryRepository.findAll();
         return null;
     }
 
