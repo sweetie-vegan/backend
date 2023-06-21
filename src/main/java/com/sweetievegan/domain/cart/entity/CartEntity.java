61c07415cfc2ecb4de9670b72dc754a76384d4c6
오채영 <web3765.gmail.com>
2023-06-21 17:57:18 +0900

    [feat] CartEntity - member 추가

--- src/main/java/com/sweetievegan/domain/cart/entity/CartEntity.java
+++ src/main/java/com/sweetievegan/domain/cart/entity/CartEntity.java
@@ -1,7 +1,9 @@
 package com.sweetievegan.domain.cart.entity;
 
+import com.sweetievegan.domain.member.entity.MemberEntity;
 import com.sweetievegan.domain.product.entity.ProductEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,9 +21,20 @@ public class CartEntity {
     private int totalCount;
     private double totalPrice;
 
-//    join members
-//    join products
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId")
+    private MemberEntity member;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "productId")
     private ProductEntity product;
+
+    @Builder
+    public CartEntity(Long cartId, int totalCount, double totalPrice, MemberEntity member, ProductEntity product) {
+        this.cartId = cartId;
+        this.totalCount = totalCount;
+        this.totalPrice = totalPrice;
+        this.member = member;
+        this.product = product;
+    }
 }
