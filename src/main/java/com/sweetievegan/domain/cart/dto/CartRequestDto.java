35e5dc8c413d63696bdb87f3b54116f1b4960ac7
오채영 <web3765.gmail.com>
2023-06-21 17:50:12 +0900

    [feat] CartDto 생성, 작성

---
+++ src/main/java/com/sweetievegan/domain/cart/dto/CartRequestDto.java
@@ -0,0 +1,15 @@
+package com.sweetievegan.domain.cart.dto;
+
+import com.sweetievegan.domain.product.entity.ProductEntity;
+import lombok.*;
+
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartRequestDto {
+    private int totalCount;
+    private double totalPrice;
+    private ProductEntity product;
+}
---
+++ src/main/java/com/sweetievegan/domain/cart/dto/CartResponseDto.java
@@ -0,0 +1,20 @@
+package com.sweetievegan.domain.cart.dto;
+
+import com.sweetievegan.domain.product.entity.ProductEntity;
+import lombok.*;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartResponseDto {
+    private Long cartId;
+    private int totalCount;
+    private double totalPrice;
+    private ProductEntity product;
+}
