ff3757d871291ff89ea713d07a11cb356d110982
오채영 <web3765.gmail.com>
2023-06-22 09:24:56 +0900

    [feat] CartController 작성

--- src/main/java/com/sweetievegan/domain/cart/controller/CartController.java
+++ src/main/java/com/sweetievegan/domain/cart/controller/CartController.java
@@ -1,13 +1,28 @@
 package com.sweetievegan.domain.cart.controller;
 
-import com.sweetievegan.domain.member.service.MemberService;
+import com.sweetievegan.domain.cart.dto.CartRequestDto;
+import com.sweetievegan.domain.cart.service.CartService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/carts")
 @RequiredArgsConstructor
 public class CartController {
-    private final MemberService memberService;
+    private final CartService cartService;
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<?> cartsByMemberId(@PathVariable("memberId") Long memberId) {
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.findCartsByMemberId(memberId));
+    }
+    @PostMapping("")
+    public ResponseEntity<?> cartRegister(@RequestBody CartRequestDto cartRequestDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.registerCart(cartRequestDto));
+    }
+    @DeleteMapping("/{cartId}")
+    public ResponseEntity<?> cartDelete(@PathVariable("cartId") Long cartId) {
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.removeCart(cartId));
+    }
 }
