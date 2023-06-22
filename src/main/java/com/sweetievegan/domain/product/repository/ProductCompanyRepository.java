66e48a1625037da6e494bdc506fa020afb2a565a
오채영 <web3765.gmail.com>
2023-06-22 10:47:39 +0900

    [fix] ProductCompany - run 오류 해결

--- src/main/java/com/sweetievegan/domain/product/repository/ProductCompanyRepository.java
+++ src/main/java/com/sweetievegan/domain/product/repository/ProductCompanyRepository.java
@@ -3,7 +3,9 @@ package com.sweetievegan.domain.product.repository;
 import com.sweetievegan.domain.product.entity.ProductCompanyEntity;
 import com.sweetievegan.domain.product.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProductCompanyRepository extends JpaRepository<ProductEntity, Long> {
+@Repository
+public interface ProductCompanyRepository extends JpaRepository<ProductCompanyEntity, Long> {
     ProductCompanyEntity findCompanyByCompanyName(String companyName);
 }
