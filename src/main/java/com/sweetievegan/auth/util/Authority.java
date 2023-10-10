393a6cec5e72bd416faafd4bd6f308154b34bd49
oh5chaeyoung <110815151+oh5chaeyoung@users.noreply.github.com>
2023-10-10 10:46:38 +0900

    feat: Implement member signup and login (#8)
    Spring Security and JWT

---
+++ src/main/java/com/sweetievegan/auth/util/Authority.java
@@ -0,0 +1,10 @@
+package com.sweetievegan.auth.util;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Authority {
+	ROLE_USER, ROLE_ADMIN
+}
